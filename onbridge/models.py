e86b3a479e34181363d7b87650db447b5b860e39
OnBridge Cook <cook@onbridge.io>
2021-10-30 16:50:49 +0300

    chore: add model

--- onbridge/models.py
+++ onbridge/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Token(models.Model):
+    token_id = models.PositiveIntegerField()
+    owner = models.CharField(max_length=255, verbose_name='Owners')
+    image = models.FileField(upload_to='images/')
+    chain_id = models.PositiveIntegerField()
+    tx = models.CharField(max_length=255, verbose_name='Tx hashes')
+    block = models.PositiveBigIntegerField()
