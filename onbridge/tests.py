044b8fc667464cbd54a2f6a6069c3de451b32186
OnBridge Cook <cook@onbridge.io>
2021-10-30 18:24:43 +0300

    test: add tests

--- onbridge/tests.py
+++ onbridge/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from . import models
+from . import serializers
+
+
+class TokenTests(APITestCase):
+
+    token_id = 1
+    owner = 'Ox01'
+    chain_id = 1
+    tx = '0x01'
+    block = 1
+
+    def setUp(self):
+        token = models.Token(
+            token_id=self.token_id,
+            owner=self.owner,
+            chain_id=self.chain_id,
+            tx=self.tx,
+            block=self.block
+        )
+        token.save()
+
+    def test_model(self):
+        token = models.Token.objects.get(id=1)
+        self.assertEqual(token.token_id, self.token_id)
+        self.assertEqual(token.owner, self.owner)
+        self.assertEqual(token.chain_id, self.chain_id)
+        self.assertEqual(token.tx, self.tx)
+        self.assertEqual(token.block, self.block)
+        self.assertEqual(models.Token.objects.count(), 1)
+
+    def test_view(self):
+        url = reverse('token-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 1)
+
+    def test_view_get_item(self):
+        response = self.client.get('/api/tokens/1/')
+        token = models.Token.objects.get(pk=1)
+        serializer = serializers.TokenSerializer(token)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_view_get_does_not_exist_item(self):
+        response = self.client.get('/api/tokens/123/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
