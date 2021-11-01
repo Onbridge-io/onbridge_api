2f05dbac77ad53f62897b5d3c5c51848f01a13c2
OnBridge Cook <cook@onbridge.io>
2021-11-02 00:22:49 +0300

    chore: use order_by token_id for Token model

--- onbridge/views.py
+++ onbridge/views.py
@@ -9,5 +9,5 @@ class TokenView(viewsets.ModelViewSet):
     lookup_field = 'token_id'
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = serializers.TokenSerializer
-    queryset = models.Token.objects.all()
+    queryset = models.Token.objects.all().order_by('token_id')
     http_method_names = ['get', 'options', 'head']
