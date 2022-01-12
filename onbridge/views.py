15538c8d64f1d64715497804b42aa9f660f55392
Alex Skipper <skipper@onbridge.io>
2022-01-12 17:12:44 +0300

    feat: add user filter to token view

--- onbridge/views.py
+++ onbridge/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 from . import models
 from . import serializers
 
 
 class TokenView(viewsets.ModelViewSet):
+    http_method_names = ['get', 'options', 'head']
     lookup_field = 'token_id'
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = serializers.TokenSerializer
     queryset = models.Token.objects.all().order_by('token_id')
-    http_method_names = ['get', 'options', 'head']
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['owner']
