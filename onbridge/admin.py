a2d673c710b902a72bde43d6bff86e864cf762d1
OnBridge Canoneer <team@onbridge.io>
2022-03-28 16:11:42 +0300

    feat: add admin fields config #ONBD-59

--- onbridge/admin.py
+++ onbridge/admin.py
@@ -4,16 +4,22 @@ from . import models
 
 
 class StatusAdmin(admin.ModelAdmin):
+    list_display = ('chain_id', 'indexed_block')
+
     class Meta:
         model = models.Status
 
 
 class TokenAdmin(admin.ModelAdmin):
+    list_display = ('token_id', 'owner', 'chain_id', 'block_number', 'skill')
+
     class Meta:
         model = models.Token
 
 
 class ActionAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'direction', 'token_id', 'status')
+
     class Meta:
         model = models.Action
 
