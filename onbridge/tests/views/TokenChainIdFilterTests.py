89efee80ef8e3d3e78313748ec15a65cfb71374f
OnBridge Canoneer <team@onbridge.io>
2022-03-30 13:56:13 +0300

    feat: filter many comma-separated chainId #ONBD-63

--- onbridge/tests/views/TokenChainIdFilterTests.py
+++ onbridge/tests/views/TokenChainIdFilterTests.py
@@ -1,7 +1,6 @@
 from datetime import date
 
 from django.test import Client, TestCase
-from rest_framework.status import HTTP_400_BAD_REQUEST
 
 from onbridge import models
 
@@ -33,7 +32,7 @@ class TokenChainIdFilterTests(TestCase):
 
     def test_successfully_filtered(self):
         for chain_id in self.tokens_generation_info:
-            response = self.client.get(f"/api/tokens/?chain_id={chain_id}")
+            response = self.client.get(f"/api/tokens/?chainId={chain_id}")
             response_in_json = response.json()
             expected_count = self.tokens_generation_info[chain_id]
             actual_count = response_in_json['count']
@@ -43,5 +42,34 @@ class TokenChainIdFilterTests(TestCase):
                 self.assertEqual(token['chainId'], chain_id)
 
     def test_filter_by_non_existent_chain_id(self):
-        response = self.client.get(f"/api/tokens/?chain_id=228")
-        self.assertEqual(HTTP_400_BAD_REQUEST, response.status_code)
+        response = self.client.get("/api/tokens/?chainId=228")
+        self.assertEqual(200, response.status_code)
+
+    def test_filter_with_comma_separated(self):
+        chain_ids = map(str, self.tokens_generation_info.keys())
+        response = self.client.get(f"/api/tokens/?chainId={','.join(chain_ids)}")
+        data = response.json()
+        self.assertEqual(data['count'], sum(self.tokens_generation_info.values()))
+
+    def test_filter_with_bad_comma_separated(self):
+        response = self.client.get("/api/tokens/?chainId=1,2,3,,,44")
+        data = response.json()
+        self.assertEqual(0, data['count'])
+
+    def test_filter_with_letters_in_query(self):
+        response = self.client.get("/api/tokens/?chainId=1,a,b,c,2,3,4")
+        data = response.json()
+        self.assertEqual(0, data['count'])
+
+    def test_filter_with_strange_chars(self):
+        response = self.client.get("/api/tokens/?chainId=^,&@,YB,FYB,&,#$H(")
+        data = response.json()
+        self.assertEqual(0, data['count'])
+
+    def test_filter_with_empty_param(self):
+        response_without_filter = self.client.get("/api/tokens/")
+        response_with_filter = self.client.get("/api/tokens/?chainId=")
+        data_without_filter = response_without_filter.json()
+        data_with_filter = response_with_filter.json()
+        self.assertEqual(data_with_filter['count'], data_without_filter['count'])
+        self.assertEqual(data_with_filter['count'], sum(self.tokens_generation_info.values()))
--- onbridge/views.py
+++ onbridge/views.py
@@ -1,18 +1,36 @@
+import logging
+
+from django.db.models import QuerySet
+from django_filters import FilterSet
+from django_filters.filters import CharFilter
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django_filters import FilterSet
-from django_filters.filters import AllValuesMultipleFilter
 
 from . import models
 from . import serializers
 
 
-class TokenFilter(FilterSet):
-    chain_id = AllValuesMultipleFilter()
+class ChainIdFilter(CharFilter):
+
+    def filter(self, qs: QuerySet, value: str):
+        if value:
+            try:
+                items = list(map(int, value.split(",")))
+                return qs.filter(chain_id__in=items)
+            except Exception as e:
+                logging.info(f"When filtering tokens by chainId error was caught: {e}")
+                return qs.none()
+        else:
+            return qs
+
+
+class TokenFilterSet(FilterSet):
+    chainId = ChainIdFilter(label='Chain ID', field_name='chain_id', lookup_expr='in')
+    skill_ability = CharFilter(field_name='skill')
 
     class Meta:
         model = models.Token
-        fields = ['chain_id']
+        fields = ['owner']
 
 
 class TokenView(viewsets.ModelViewSet):
@@ -21,4 +39,4 @@ class TokenView(viewsets.ModelViewSet):
     serializer_class = serializers.TokenSerializer
     queryset = models.Token.objects.all().order_by('token_id')
     http_method_names = ['get', 'options', 'head']
-    filterset_class = TokenFilter
+    filterset_class = TokenFilterSet
