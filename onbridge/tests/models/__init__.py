e890fb1d449f128bdc2d926ef1598457b6e2a752
OnBridge Cook <cook@onbridge.io>
2022-03-31 12:52:56 +0300

    feat: add actions endpoint #ONBD-72

--- onbridge/tests/models/__init__.py
+++ onbridge/tests/models/__init__.py
@@ -1,1 +1,2 @@
 from .StatusTests import *
+from .ActionTests import *
--- onbridge/tests/views/__init__.py
+++ onbridge/tests/views/__init__.py
@@ -1,3 +1,4 @@
 from .TokenTests import *
 from .TokenChainIdFilterTests import *
 from .TokenOwnerFilterTests import *
+from .ActionTests import *
