ef86209d773cf02eebd139870b8f3c68bc4580b9
OnBridge Canoneer <team@onbridge.io>
2022-04-01 13:45:41 +0300

    test: Indexer's method get_token_metadata_from_contract #ONBD-86

---
+++ onbridge/tests/indexer/ReceiveTokenMetadataTests.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+
+from .mock_envs import *
+from token_indexer.Indexer import Indexer, IndexerException
+
+
+class ReceiveTokenMetadataTests(TestCase):
+
+    def setUp(self) -> None:
+        self.indexer = Indexer(
+            upstream=UPSTREAM,
+            token_abi_filename=TOKEN_ABI_FILENAME,
+            token_address=TOKEN_ADDRESS,
+            bridge_address=BRIDGE_ADDRESS,
+            indexer_interval=INDEXER_INTERVAL,
+            ipfs_host=IPFS_HOST
+        )
+
+    def test_successfully_received_metadata(self):
+        metadata = self.indexer.get_token_metadata_from_contract(0)
+        self.assertEqual(metadata, {'name': 'Pirate', 'description': 'Pirate that shines',
+                                    'image': 'https://ipfs.io/ipfs/QmcsXTvsFrZXBLUsUuSWyP8x18xwhCgnmD8KwsijtuXk1e/0.png',
+                                    'attributes': [{'trait_type': 'name', 'value': 'just noname pirate'}]}
+                         )
+
+    def test_fault_on_bad_token_id(self):
+        self.assertRaises(
+            IndexerException,
+            lambda:
+            self.indexer.get_token_metadata_from_contract(2 ** 257)
+        )
+
+    def test_fault_on_negative_token_id(self):
+        self.assertRaises(
+            IndexerException,
+            lambda:
+            self.indexer.get_token_metadata_from_contract(-1)
+        )
+
+    def test_mock_tokenURI_method(self):
+        """
+        Mocking indexer's contract. We replace function tokenURI to see behaviour of IPFS provider
+        """
+        mocked_indexer = Indexer(
+            upstream=UPSTREAM,
+            token_abi_filename=TOKEN_ABI_FILENAME,
+            token_address=TOKEN_ADDRESS,
+            bridge_address=BRIDGE_ADDRESS,
+            indexer_interval=INDEXER_INTERVAL,
+            ipfs_host=IPFS_HOST
+        )
+        # Now contract will tell us wrong tokenURI
+        mocked_indexer.token_contract.functions.tokenURI = lambda token_id: f"google.com/{token_id}"
+        self.assertRaises(
+            IndexerException,
+            lambda: mocked_indexer.get_token_metadata_from_contract(0)
+        )
--- onbridge/tests/indexer/__init__.py
+++ onbridge/tests/indexer/__init__.py
@@ -1,1 +1,2 @@
 from .TokenFromAdressAndABITests import *
+from .ReceiveTokenMetadataTests import *
