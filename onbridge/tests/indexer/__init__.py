dc2b2569b074b911ed69a42377921cdaaca0056d
OnBridge Canoneer <team@onbridge.io>
2022-04-01 13:21:52 +0300

    test: Indexer's method get_token_by_address_and_abi #ONBD-86

---
+++ onbridge/tests/indexer/__init__.py
@@ -0,0 +1,1 @@
+from .TokenFromAdressAndABITests import *
---
+++ onbridge/tests/indexer/mock_envs.py
@@ -0,0 +1,6 @@
+UPSTREAM = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+TOKEN_ABI_FILENAME = "token_indexer/erc721-abi.json"
+TOKEN_ADDRESS = "0xe6847645B1832B2923e5938ec482f2b0EfA6DE4c"
+BRIDGE_ADDRESS = "0x099e3307be3b694e8C7dBc54E2ecB8897806BD2A"
+INDEXER_INTERVAL = 10
+IPFS_HOST = 'https://ipfs.io'
